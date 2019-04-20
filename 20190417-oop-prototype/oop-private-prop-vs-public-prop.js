a783b4dffccaf2432a83975b64510f91551bd0cb
Raymond Linweimin <linr4@outlook.com>
2019-04-20 23:31:09 +0800

    js class, encap, inherit ...

--- 20190417-oop-prototype/oop-private-prop-vs-public-prop.js
+++ 20190417-oop-prototype/oop-private-prop-vs-public-prop.js
@@ -4,7 +4,9 @@ function Person() {
 }
 
 let p1 = new Person();
-p1.age = 20;
 
-console.log(p1.name); // John
-console.log(p1.age);  // undefined
+
+console.log(p1.name); // John // 在构造函数中，用 “this.属性” 的方式创建的为公有属性，外部可直接引用
+console.log(p1.age);  // undefined  // 用 “let 属性 = 值;” 的方式创建的为私有属性，外部无法直接访问
+p1.age = 20;  // 虽然与私有属性同名，但它是实例属性
+console.log(p1.age); // 20
